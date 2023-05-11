2525760de85bc47c4dade4906e5890aa5c5d9e90
MichalMolik <molson.and@gmail.com>
2023-05-11 18:45:43 +0200

    added serializer

--- api/api/views.py
+++ api/api/views.py
@@ -5,35 +5,37 @@ from rest_framework.response import Response
 from django.http import HttpResponse
 from rest_framework import viewsets, status
 from django.core.cache import cache
+from rest_framework.parsers import JSONParser
 from django_redis import get_redis_connection
+from .serializers import TableSerializer
 
 
 class TableViewSet(viewsets.ViewSet):
     def list(self, request):
         db = get_redis_connection("default")
-        table_list = db.get
-        return Response({"msg": "table list"})
+        table_list = db.keys("table:*")
+        table_values = db.mget(table_list)
+        return Response({"tables": zip(table_list, table_values)})
 
     def create(self, request):
         db = get_redis_connection("default")
-        game_master_key = "game_master" + request.data.get("game_master_id")
-        game_master_stack = request.data.get("stack") or 0
-        table_key = "table" + request.data.get("table_id")
-        if db.get(game_master_key) is None:
-            db.set(game_master_key, game_master_stack)
-        else:
-            return Response({"ERROR": "this game master exists"})
-        if db.get(table_key) is None:
-            db.sadd(table_key, game_master_key)
+        data = JSONParser().parse(request)
+        serializer = TableSerializer(data=data)
+        if serializer.is_valid():
+            game_master_key = f"""game_master:{serializer.data.get("game_master_id")}"""
+            game_master_stack = serializer.data.get("stack") or 0
+            table_key = f"""table:{serializer.data.get("table_id")}"""
 
-        print(cache.get("table:1:data"))
-        try:
-            print(request.data.get("msg"))
+            if db.get(game_master_key) is None:
+                db.set(game_master_key, game_master_stack)
+            else:
+                return Response({"ERROR": "this game master exists"})
+            if db.get(table_key) is None:
+                db.sadd(table_key, game_master_key)
+            else:
+                return Response({"ERROR": "this table exists"})
 
-        except Exception as e:
-            print(e)
-            return Response({"error": "check content"})
-        return Response({"table": "table"})
+        return Response({"table": "create"})
 
 
 class PlayerViewSet(viewsets.ViewSet):
