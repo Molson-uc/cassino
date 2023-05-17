8329686766402b60050a98e5113b0bc2a6534417
MichalMolik <molson.and@gmail.com>
2023-05-17 18:22:32 +0200

    clean up urls

--- api/api/urls.py
+++ api/api/urls.py
@@ -24,12 +24,8 @@ from . import views
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Cassino API",
         default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
     ),
     public=True,
 )
@@ -43,17 +39,9 @@ router.register(r"transactions", views.TransactionViewSet, basename="transaction
 
 urlpatterns = [
     re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    re_path(
         r"^swagger/$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
-    ),
-    re_path(
-        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
 ]
 urlpatterns += staticfiles_urlpatterns()
