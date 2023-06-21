ec590a2e711039500c597a76a81851dad689df27
MichalMolik <molson.and@gmail.com>
2023-06-21 20:18:57 +0200

    remove caches settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -48,17 +48,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = []
 REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": f"redis://{REDIS_HOST}:6379/0",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-    }
-}
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": f"redis://{REDIS_HOST}:6379/0",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         },
+#     }
+# }
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# SESSION_CACHE_ALIAS = "default"
 
 ROOT_URLCONF = "api.urls"
 
